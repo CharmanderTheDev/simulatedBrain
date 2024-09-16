2cee54f219559a3b282450e7a2e06685bb2f61f0
CharmanderTheDev <144705748+CharmanderTheDev@users.noreply.github.com>
2024-09-16 08:02:46 -0500

    more progress on rust transition

--- simulatedBrain/src/main.rs
+++ simulatedBrain/src/main.rs
@@ -10,6 +10,11 @@ struct Synapse {
     transmitters: f32,
 } impl Synapse {
 
+    fn new(input: Neuron, output: Neuron, id: usize) -> Synapse{
+        let mut synapse = Synapse{input:input,output:output,id:id,weight:0.0,transmitters:0.0}
+        synapse.set_weight(1.0)
+    }
+
     fn tick(mut self) {
         self.output.add_potential(self.transmitters);
         self.transmitters = 0.0;
@@ -25,11 +30,11 @@ struct Synapse {
         self.transmitters += amount;
     }
 
-    fn adjustWeight(mut self, dopamine: f32, heat: f32){
-        self.set_weight(self.weight/(
-                              (1.0 as f32)/
-        (self.input.get_synapse_amount()*self.weight).pow(dopamine * heat)));
+    fn adjust_weight(mut self, dopamine: f32, heat: f32) {
+        self.set_weight(self.weight/(((1.0 as f32)/(self.input.get_synapse_amount()*self.weight)).powf(dopamine * heat)));
     }
+
+    fn get_weight(self) -> f32 {self.weight}
 }
 
 struct Neuron {
@@ -39,14 +44,47 @@ struct Neuron {
 
     heat: f32,
     potential: f32,
-    synapses: HashMap<usize, Synapse,>,
+    synapses: HashMap<usize, Synapse>,
 
     synapse_amount: usize,
     weight_total: f32,
 }
 impl Neuron {
 
-    
+    fn new(brain: Brain, id: usize) -> Neuron{
+        Neuron {
+            brain:brian, 
+            id:id, 
+            heat:0.0, 
+            potential:0.0, 
+            synapses:HashMap::new(), 
+            synapse_amount:0, 
+            weight_total:0.0
+        };
+    }
+
+    fn normalize(self) {
+        for (id, synapse) in self.synapses {
+            synapse.setWeight(synapse.getWeight/self.weight_total);
+        }
+
+        self.weight_total = self.synapse_amount;
+    }
+
+    fn add_potential(self, amount:f32) {
+        self.potential += amount;
+    }
+
+    fn fire(self) {
+        self.normalize();
+        for (id, synapse) in self.synapses {
+            synapse.transmit(synapse.get_weight());
+        }
+    }
+
+    fn addSynapse(self, synapse: Synapse) {
+        
+    }
 }
 
 struct Brain {}
---
+++ simulatedBrain/target/debug/build/num-traits-da869c0611ee78c4/build_script_build-da869c0611ee78c4.d
@@ -0,0 +1,5 @@
+C:\Users\victo\Documents\GitHub\simulatedBrain\simulatedBrain\target\debug\build\num-traits-da869c0611ee78c4\build_script_build-da869c0611ee78c4.exe: C:\Users\victo\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-traits-0.2.19\build.rs
+
+C:\Users\victo\Documents\GitHub\simulatedBrain\simulatedBrain\target\debug\build\num-traits-da869c0611ee78c4\build_script_build-da869c0611ee78c4.d: C:\Users\victo\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-traits-0.2.19\build.rs
+
+C:\Users\victo\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-traits-0.2.19\build.rs:
